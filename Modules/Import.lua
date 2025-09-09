5cfa26d06c2df543af3dc53838358c26e97f00c9
moon-rblx <moonrblx@icloud.com>
2025-09-09 18:23:03 -0500

    Delete Modules directory

--- Modules/Import.lua
+++
@@ -1,9 +0,0 @@
-local Sea = workspace:GetAttribute('MAP')
-
-local Modules = {}
-local BaseURL = 'https://raw.githubusercontent.com/'
-
-Modules.Mobs = loadstring(game:HttpGet(BaseURL .. 'moon-rblx/Project-Madara/refs/heads/main/Data/994732206/Sea1/Mobs.lua'))()
-
-
-return Modules
